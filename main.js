44e6d97917fd9776435e3add6da87f2ae710022f
Richard Kraaijenhagen <stdin@riichard.com>
2017-08-10 13:46:25 +0200

    Added fromHtml feature

--- main.js
+++ main.js
@@ -8,13 +8,18 @@ var fs = require('fs');
 var cli = require('cli');
 var util = require('util');
 var htmlparser = require('htmlparser');
+var rfr = require('rfr');
+var parser = {
+    toMarkup: rfr('src/toMarkup')
+};
+
+
 
 var options = cli.parse({
     toHtml: ['m', 'Markup file to convert to HTML', 'file', false],
     fromHtml: ['html', 'HTML file to convert to Markup', 'file', false]
 });
 
-console.log(options);
 
 // In: HTML
 // Out: err, Data structure
@@ -28,7 +33,7 @@ function parseHtml(html, callback) {
 
     var parser = new htmlparser.Parser(handler);
     parser.parseComplete(html);
-    console.log(util.inspect(handler.dom, false, null));
+    return handler.dom;
 }
 
 // In: Markup
@@ -37,14 +42,14 @@ function parseMarkup(markup, callback) {
 }
 
 
-
 if(options.fromHtml) {
     fs.readFile(options.fromHtml,'utf8', function(err, data) {
         if(err) {
             return console.log(err);
         }
         var data = parseHtml(data);
-        console.log(data);
+        var markup = parser.toMarkup(data);
+        console.log(markup);
     });
 
 }
---
+++ src/toMarkup.js
@@ -0,0 +1,58 @@
+
+
+var formatting = {
+    // Returns string of node
+    section: () => ['', '\n'],
+    h2: () => ['== ', ' ==\n'],
+
+    // TODO ol - better support for numbered items. Perhaps move children
+    // concatenation to each formatting function. These numbers will bug if a non-li
+    // node is part of the list
+    li: (node, parent, i) => [(parent.name === 'ol' ? (i+1)+'.' : '*') + ' ', '\n'],
+
+    // TODO ol/li: add support for nested items
+    ol: () => ['','']
+
+    // TODO add other formattings
+};
+
+function nodeToString(node, parent, index) {
+    // No need for formatting on text nodes
+    if(node.type === 'text') return node.data;
+
+    // Convert children to string if existing
+    var childrenString = node.children ? nodesToString(node.children, node) : '';
+
+    // If we have formatting for this type of node, add it and return as if
+    if(node.type === 'tag' && typeof formatting[node.name] !== 'undefined') {
+        var markup = formatting[node.name](node, parent, index);
+
+        return [markup[0], childrenString, markup[1]].join('');
+    }
+
+    return childrenString;
+}
+
+function nodesToString(nodes, parent) {
+    // Leave the formatting to the formatting functions, whitespace nodes will
+    // only disturb our formatting
+    return removeEmptyTextNodes(nodes)
+
+        // Convert array of nodes to array of strings
+        .map((node, i) => nodeToString(node, parent, i))
+
+        // Convert array of strings to string
+        .join('');
+}
+
+// Returns array of nodes, without the text nodes that only include whitespace
+function removeEmptyTextNodes(nodes) {
+    return nodes.filter((node) => {
+        if(node.type === 'text') {
+            return !(/^\s+$/.test(node.data))
+        }
+        return true;
+    });
+}
+
+module.exports = nodesToString;
