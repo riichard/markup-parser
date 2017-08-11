180957b4af51cee1140a8b570b13a9b7df4a7cb9
Richard Kraaijenhagen <stdin@riichard.com>
2017-08-11 17:50:16 +0200

    Added deleteNode and addNodeAtIndex functions to diff tool

--- src/diff.js
+++ src/diff.js
@@ -4,18 +4,46 @@ var util = require('util');
 // In: dom object
 // Edits the dom object by reference, does not return it
 function applyDiff(diff, nodes) {
-    // TODO create parser for diff markup. First step is having the features to
-    // edit the dom
-    //console.log(util.inspect(nodes, false, null));
+    // TODO create parser for diff markup. First step is to have the features to
+    // edit the dom incrementally.
     replaceNodeText(nodes, 3, 'Hallo Wereld');
-    //console.log(util.inspect(nodes, false, null));
+    replaceNodeText(nodes, 0, 'Nieuwe titel');
+    deleteNode(nodes, 2);
+    addNodeAtIndex(nodes, 1, {
+        type: 'tag',
+        name: 'li',
+        children: [
+            { type: 'text', data: 'We know nothing' }
+        ]
+    });
 }
 
 function replaceNodeText(dom, index, newText) {
     var node = findNode(dom, index);
     console.log('node', index, node);
 
+    // TODO This assumes that nodes can only have one text child.
+    node.children = [{
+        type: 'text',
+        data: newText
+    }];
 }
+
+// Deletes node with $index from the dom
+function deleteNode(dom, index) {
+    var node = findNode(dom, index);
+    node.siblings.splice(node.indexToParent, 1);
+}
+
+// Adds node at $index
+function addNodeAtIndex(dom, index, newNode) {
+    console.log('adding nodes');
+    var node = findNode(dom, index);
+    console.log(node);
+    node.siblings.splice(node.indexToParent, 0, newNode);
+    console.log(util.inspect(dom, false, null));
+}
+
 
 // In: Dom
 // In: Index
@@ -24,29 +52,16 @@ function replaceNodeText(dom, index, newText) {
 function findNode(dom, index) {
     for(var i=0;i<dom.length;i++){
         var node = dom[i];
+
+        // Setting data for book-keeping
+        node.siblings = dom;
+        node.indexToParent = i;
+
         var currentIndex = node.attribs ? node.attribs.line : -1;
 
         if(currentIndex === index) {
             return node;
         }
-
-        //if(i === dom.length -1) {
-            //console.log('node is last sibling');
-            //var nextSiblingIndex = Infinity;
-        //}
-        //else {
-            //var nextSibling = dom[i + 1];
-            //var nextSiblingIndex = nextSibling.attribs ? parseInt(nextSibling.attribs.line) : -1;
-            //console.log('next sibling:', nextSiblingIndex, nextSibling);
-        //}
-
-        // Look in this node's children if the next sibling has a greater index
-        //if(index < nextSiblingIndex) {
-            //console.log('node is before next index');
-            //console.log(node);
-            //if(!node.children) return -1;
-            //return findNode(node.children, index);
-        //}
 
         if(node.children) {
             var nodeInChildren = findNode(node.children, index);
@@ -57,6 +72,10 @@ function findNode(dom, index) {
     }
     return -1;
 }
+
+// TODO if we have a feature that can generate and set the indexes on each
+// node, we're not dependant on any other code changing our numberings. We can
+// also keep that code clean of tracking indexes.
 
 
 module.exports = {
